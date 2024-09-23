9961f1bca94a5d98470cadcf8b030b83eb10baa9
PavelSvec277 <svecpavel@centrum.cz>
2024-09-23 20:24:01 +0200

    prvni zapis

--- Projekt_2/test_1/test_web_1.py
+++ Projekt_2/test_1/test_web_1.py
@@ -1,31 +1,10 @@
 import pytest
 import playwright
 
-@pytest.fixture()
-def browser():
 
-    with playwright():
-        
-
-        browser = playwright.chromium.launch(
-            headless=False, slow_mo=3000
-        )  
-
-        yield browser
-
-        browser.close()
-
-@pytest.fixture()
-def page(browser):
-
-    page = browser.new_page()
-
-    yield page
-
-    page.close()
 
 def test_idos(page):
      page.goto("https://idos.cz/vlakyautobusymhdvse/spojeni/")
-     logo = page.locator(h1 id= "logo")
+     logo = page.locator(id= "logo")
 
      assert logo == True
