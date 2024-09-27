afb41f96c0e8b87518c9759c368c0b86fea81f45
PavelSvec277 <svecpavel@centrum.cz>
2024-09-27 13:35:26 +0200

    beckup

--- Projekt_2/test_1/test_web_1.py
+++ Projekt_2/test_1/test_web_1.py
@@ -1,11 +1,11 @@
 import pytest
 from playwright.sync_api import Page
-from Projekt_2.home_page import Home_page
+from home_page import Home_page
 
 
 
 def test_engeto_logo_click(page):
      page.goto("https://engeto.cz/")
      logo = page.locator(f'div.a.class:has-text("logo-link")')
-     expect(logo).to_be_visible()
+     logo.to_be_visible()
      assert logo.click()
