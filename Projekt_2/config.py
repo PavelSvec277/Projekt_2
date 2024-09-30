e0c4626c78375d24adf2ec894005a1cf997d0abd
PavelSvec277 <svecpavel@centrum.cz>
2024-09-30 14:00:15 +0200

    config oprava

--- Projekt_2/config.py
+++ Projekt_2/config.py
@@ -1,20 +1,23 @@
 import pytest
-from playwright.sync_api import Playwright, sync_playwright
+from playwright.sync_api import sync_playwright
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo = 6000)
+        browser = p.chromium.launch(
+            headless=False, slow_mo=6000
+            )
         yield browser
         browser.close()
 
-@pytest.fixture
-def context(browser):
-    context = browser.new_content(
-        viewport={'width': 1920, 'height': 1080}
-    )
-    yield context
-    context.close()
+@pytest.fixture()
+def page(browser):
+
+    page = browser.new_page()
+        browser.window={'width': 1920, 'height': 1080}
+
+    yield page
+    page.close()
 
 @pytest.fixture
 def page(context):
